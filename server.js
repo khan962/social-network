16ff7e497f2d7732fd3206ca73a6c5c9dac10ae8
khan962 <akinur.syl@gmail.com>
2022-01-02 19:08:42 +0600

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+
+//dsb connection
+const connectDB = require("./config/db");
+connectDB();
+
+app.get("/", (req, res) => res.send("App running"));
+const PORT = process.env.PORT || 5000;
+
+// Define routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/post", require("./routes/api/post"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/auth", require("./routes/api/auth"));
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
