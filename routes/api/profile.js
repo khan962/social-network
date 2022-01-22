2a7f93218bdac7ad2ba57486c19d5cfd1f356d19
khan962 <akinur.syl@gmail.com>
2022-01-23 01:42:06 +0600

    All/Single profile get, Profile Update/Delete

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -201,4 +201,45 @@ router.put(
   }
 );
 
+// @route   PUT /api/profile/education
+// @desc    Add profile education
+// @access  Private
+router.put(
+  "/education",
+  [
+    auth,
+    check("school", "School is required").not().isEmpty(),
+    check("degree", "Degree is required").not().isEmpty(),
+    check("from", "From date is required").not().isEmpty(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { school, degree, from, to, current, description } = req.body;
+
+    const newEdu = {
+      school,
+      degree,
+      from,
+      to,
+      current,
+      description,
+    };
+
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+      profile.education.unshift(newEdu);
+      await profile.save();
+
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Internal server error");
+    }
+  }
+);
+
 module.exports = router;
