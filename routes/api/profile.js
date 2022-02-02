4e95975368d26e8f3307c6b893409e2b19d14eb4
khan962 <akinur.syl@gmail.com>
2022-02-02 21:54:55 +0600

    get all git repo by username

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -289,4 +289,34 @@ router.delete("/education/:edu_id", auth, async (req, res) => {
   }
 });
 
+// @route   GET /api/profile/github/:username
+// @desc    Get user repositories from Github
+// @access  Public
+router.get("/github/:username", (req, res) => {
+  try {
+    const options = {
+      uri: `https://api.github.com/users/${
+        req.params.username
+      }/repos?per_page=5&sort=created&direction=asc&client_id=${config.get(
+        "githubClientId"
+      )}&client_secret=${config.get("githubSecret")}`,
+      method: "GET",
+      headers: { "user-agent": "node.js" },
+    };
+
+    request(options, (error, response, body) => {
+      if (error) console.error(error);
+
+      if (response.statusCode !== 200) {
+        return res.status(404).json({ msg: "Github profile not found" });
+      }
+
+      return res.json(JSON.parse(body));
+    });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("internal server error");
+  }
+});
+
 module.exports = router;
