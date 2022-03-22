a59d4d7abc95760ff4fd646b023221c0c4082705
khan962 <akinur.syl@gmail.com>
2022-03-22 21:18:35 +0600

    GET ALL DEV PROFILE

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import EditProfile from "./components/profile-forms/EditProfile";
 import AddExperience from "./components/profile-forms/AddExperience";
 import AddEducation from "./components/profile-forms/AddEducation";
 import Profiles from "./components/profiles/Profiles";
+import Profile from "./components/profile/Profile";
 // Redux
 import { Provider } from "react-redux";
 import store from "./store";
@@ -21,73 +22,74 @@ import store from "./store";
 import "./App.css";
 
 if (localStorage.token) {
-	setAuthToken(localStorage.token);
+  setAuthToken(localStorage.token);
 }
 
 const App = () => {
-	useEffect(() => {
-		store.dispatch(loadUser());
-	}, []);
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
 
-	return (
-		<Provider store={store}>
-			<BrowserRouter>
-				<Navbar />
-				<Alert />
-				<Routes>
-					<Route exact path="/" element={<Landing />} />
-					<Route exact path="/register" element={<Register />} />
-					<Route exact path="/login" element={<Login />} />
-					<Route exact path="/profiles" element={<Profiles />} />
-					<Route
-						exact
-						path="/dashboard"
-						element={
-							<PrivateRoute>
-								<Dashboard />
-							</PrivateRoute>
-						}
-					/>
-					<Route
-						exact
-						path="/create-profile"
-						element={
-							<PrivateRoute>
-								<CreateProfile />
-							</PrivateRoute>
-						}
-					/>
-					<Route
-						exact
-						path="/edit-profile"
-						element={
-							<PrivateRoute>
-								<EditProfile />
-							</PrivateRoute>
-						}
-					/>
-					<Route
-						exact
-						path="/add-experience"
-						element={
-							<PrivateRoute>
-								<AddExperience />
-							</PrivateRoute>
-						}
-					/>
-					<Route
-						exact
-						path="/add-education"
-						element={
-							<PrivateRoute>
-								<AddEducation />
-							</PrivateRoute>
-						}
-					/>
-				</Routes>
-			</BrowserRouter>
-		</Provider>
-	);
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <Navbar />
+        <Alert />
+        <Routes>
+          <Route exact path="/" element={<Landing />} />
+          <Route exact path="/register" element={<Register />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/profiles" element={<Profiles />} />
+          <Route exact path="/profile/:id" element={<Profile />} />
+          <Route
+            exact
+            path="/dashboard"
+            element={
+              <PrivateRoute>
+                <Dashboard />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            exact
+            path="/create-profile"
+            element={
+              <PrivateRoute>
+                <CreateProfile />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            exact
+            path="/edit-profile"
+            element={
+              <PrivateRoute>
+                <EditProfile />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            exact
+            path="/add-experience"
+            element={
+              <PrivateRoute>
+                <AddExperience />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            exact
+            path="/add-education"
+            element={
+              <PrivateRoute>
+                <AddEducation />
+              </PrivateRoute>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  );
 };
 
 export default App;
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,9 +1,7 @@
 const config = require("config");
 const jwt = require("jsonwebtoken");
-
 module.exports = function (req, res, next) {
   const token = req.header("x-auth-token");
-
   //checking token
   if (!token) {
     return res.status(401).json({
